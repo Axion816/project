aba20b25031d08e8877089d3c123c5cfbfda2b2d
Axion816 <60110445+Axion816@users.noreply.github.com>
2020-05-19 00:04:19 +0100

    Attempt to modulise

---
+++ BallMovement.java
@@ -0,0 +1,23 @@
+public class BallMovement{
+    //Movement
+    private double velocityX = 1.00;
+    private double velocityY = 2.00;
+    private double friction = 0.80;
+    //Table Interaction
+    public void moveCueBall(Ball b){
+        b.move(velocityX*friction,velocityY*friction);
+            //Conditions
+            if(b.getXPosition()>=1239.00){
+                velocityX = -1.00*velocityX;
+            }
+            if(b.getXPosition()<=261.00){
+                velocityX = -1.00*velocityX;
+            }
+            if(b.getYPosition()>=739.00){
+                velocityY = -1.00*velocityY;
+            }
+            if(b.getYPosition()<=261.00){
+                velocityY = -1.00*velocityY;
+            }
+    }
+}
