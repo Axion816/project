5f40e7530050f7d42b66b640350ae7b853f9792c
Axion816 <60110445+Axion816@users.noreply.github.com>
2020-05-18 14:48:56 +0100

    Fix ratio of table

--- Driver.java
+++ Driver.java
@@ -1,6 +1,6 @@
 public class Driver{
     public static void main(String[] Args){
-        GameArena arena = new GameArena(2000,1000);
+        GameArena arena = new GameArena(1500,1000);
         /*Test
         Ball a = new Ball(50.00,50.00,10.00,"RED");
         arena.addBall(a);
@@ -9,8 +9,8 @@ public class Driver{
         Text title = new Text("Welcome to Cool Pool!",14,20.00,20.00,"WHITE");
         arena.addText(title);
         //Pool Table
-        Rectangle table = new Rectangle(330.00,165.00,1340.00,670.00,"GRAY",1);
-        Rectangle felt = new Rectangle(500.00,250.00,1000.00,500.00,"GREEN",2);
+        Rectangle table = new Rectangle(330.00,165.00,1170.00,670.00,"GRAY",1);
+        Rectangle felt = new Rectangle(250.00,250.00,1000.00,500.00,"GREEN",2);
         arena.addRectangle(table);
         arena.addRectangle(felt);
         //Outlay
