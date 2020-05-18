afb08e036bd98e80f7030e0f68c7aba77c91ab6f
Axion816 <60110445+Axion816@users.noreply.github.com>
2020-05-18 12:29:38 +0100

    Aspect Fix 12:29 18/05

--- Driver.java
+++ Driver.java
@@ -1,6 +1,6 @@
 public class Driver{
     public static void main(String[] Args){
-        GameArena arena = new GameArena(500,707);
+        GameArena arena = new GameArena(707,500);
         Ball a = new Ball(50.00,50.00,10.00,"RED");
         arena.addBall(a);
 
