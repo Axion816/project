b85135dc8cce0c9bff925630ac5d3267c4b08083
Axion816 <60110445+Axion816@users.noreply.github.com>
2020-05-19 00:49:27 +0100

    Add files via upload

--- BallMovement.java
+++ BallMovement.java
@@ -4,16 +4,16 @@ public class BallMovement{
     //Table Interaction
     public void moveCueBall(Ball b,double vx,double vy){
             //Conditions
-            if(b.getXPosition()>=1239.00){
+            if(b.getXPosition()+vx>=1239.00){
                 vx = -1.00*vx;
             }
-            if(b.getXPosition()<=261.00){
+            if(b.getXPosition()-vx<=261.00){
                 vx = -1.00*vx;
             }
-            if(b.getYPosition()>=739.00){
+            if(b.getYPosition()+vy>=739.00){
                 vy = -1.00*vy;
             }
-            if(b.getYPosition()<=261.00){
+            if(b.getYPosition()-vy<=261.00){
                 vy = -1.00*vy;
             }
             b.move(vx,vy);
