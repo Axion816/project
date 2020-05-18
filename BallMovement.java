65278971721338815f7b0d6af4907e379fc3d12a
Axion816 <60110445+Axion816@users.noreply.github.com>
2020-05-19 00:25:51 +0100

    Add files via upload

--- BallMovement.java
+++ BallMovement.java
@@ -5,7 +5,6 @@ public class BallMovement{
     private double friction = 0.80;
     //Table Interaction
     public void moveCueBall(Ball b){
-        b.move(velocityX,velocityY);
             //Conditions
             if(b.getXPosition()>=1239.00){
                 velocityX = -1.00*velocityX;
@@ -19,5 +18,6 @@ public class BallMovement{
             if(b.getYPosition()<=261.00){
                 velocityY = -1.00*velocityY;
             }
+            b.move(velocityX,velocityY);
     }
 }
