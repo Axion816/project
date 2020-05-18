8e3b02a8ee866d272998f906feb02554f3d4c4f2
Axion816 <60110445+Axion816@users.noreply.github.com>
2020-05-18 20:00:03 +0100

    Ball Moving, escape removed

--- Driver.java
+++ Driver.java
@@ -17,8 +17,11 @@ public class Driver{
         arena.addRectangle(powerBar);
         Line headSpot = new Line(500.00,251.99,500.00,748.99,5.00,"WHITE",3);
         arena.addLine(headSpot);
-        if(arena.escPressed()==true){
-            arena.exit();
+        //Movement
+        while(true){
+            private double speedX = 2.00;
+            private double speedY = 0.00;
+            cueBall.move(speedX,speedY);
         }
     }
 }
