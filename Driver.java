2b430aa93fef9307946b0398ca48a297f009ce76
Axion816 <60110445+Axion816@users.noreply.github.com>
2020-05-18 13:31:27 +0100

    Headline and ball test

--- Driver.java
+++ Driver.java
@@ -1,8 +1,12 @@
 public class Driver{
     public static void main(String[] Args){
         GameArena arena = new GameArena(707,500);
+        //Test
         Ball a = new Ball(50.00,50.00,10.00,"RED");
         arena.addBall(a);
-
+        //Title
+        Text title = new Text(50.00,50.00,1,"Welcome To Cool Pool!","WHITE");
+        arena.addText(title);
+        
     }
 }
