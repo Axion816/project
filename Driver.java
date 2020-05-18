bc7f98b5009d4f1a6b54c0ce927d679172a66465
Axion816 <60110445+Axion816@users.noreply.github.com>
2020-05-18 12:12:46 +0100

    Commit 12:12 18/05
    add ball to arena

--- Driver.java
+++ Driver.java
@@ -1,5 +1,7 @@
 public class Driver{
     public static void main(String[] Args){
         GameArena arena = new GameArena(400,400);
+        Ball a = new Ball(50.00,50.00,10.00,"RED");
+        arena.addBall(a);
     }
 }
