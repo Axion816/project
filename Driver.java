e9a504c8e181a4f5266d55df577c7139f1e841fa
Axion816 <60110445+Axion816@users.noreply.github.com>
2020-05-18 14:34:51 +0100

    Table, Power bar commit
    Resized and added new content

--- Driver.java
+++ Driver.java
@@ -1,14 +1,20 @@
 public class Driver{
     public static void main(String[] Args){
-        GameArena arena = new GameArena(707,500);
-        //Test
+        GameArena arena = new GameArena(2000,1000);
+        /*Test
         Ball a = new Ball(50.00,50.00,10.00,"RED");
         arena.addBall(a);
-        //Title
+        */
+        //Text Title
         Text title = new Text("Welcome to Cool Pool!",14,20.00,20.00,"WHITE");
         arena.addText(title);
         //Pool Table
+        Rectangle table = new Rectangle(330.00,165.00,1340.00,670.00,"GRAY");
         Rectangle felt = new Rectangle(100.00,100.00,400.00,200.00,"GREEN");
+        arena.addRectangle(table);
         arena.addRectangle(felt);
+        //Outlay
+        Rectangle powerBar = new Rectangle(650.00,825.00,700.00,100.00,"RED");
+        arena.addRectangle(powerBar);
     }
 }
