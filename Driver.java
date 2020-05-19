e00f27700deb797e161deb9079a67ef9c96f4d54
Axion816 <60110445+Axion816@users.noreply.github.com>
2020-05-19 14:14:56 +0100

    Add files via upload

--- Driver.java
+++ Driver.java
@@ -1,8 +1,8 @@
 public class Driver{
     public static void main(String[] Args){
-        GameArena arena = new GameArena(1500,1000);
+        GameArena arena = new GameArena(1800,1000);
         //Balls
-        Ball cueBall = new Ball(500.00,500.00,22.00,"WHITE",4);
+        Ball cueBall = new Ball(650.00,500.00,22.00,"WHITE",4);
         arena.addBall(cueBall);
         //Text Title
         Text title = new Text("Welcome to Cool Pool!",14,20.00,20.00,"WHITE");
@@ -11,24 +11,24 @@ public class Driver{
 
         //Pool Table
         Rectangle table = new Rectangle(165.00,165.00,1170.00,670.00,"GREY",1);
-        Rectangle felt = new Rectangle(250.00,250.00,1000.00,500.00,"GREEN",2);
+        Rectangle felt = new Rectangle(400.00,250.00,1000.00,500.00,"GREEN",2);
         //Ball Pots
         Ball[] pot = new Ball[6];
         for(int i=0;i<6;i++){
             if(i<3){
-                pot[i] = new Ball(250.00+(i*500.00),250.00,49.00,"BLACK",5);
+                pot[i] = new Ball(400.00+(i*500.00),250.00,49.00,"BLACK",5);
             }
             if(i>=3){
-                pot[i] = new Ball(((i-3)*(500.00))+250.00,750.00,49.00,"BLACK",5);
+                pot[i] = new Ball(((i-3)*(500.00))+400.00,750.00,49.00,"BLACK",5);
             }
             arena.addBall(pot[i]);
         }
         arena.addRectangle(table);
         arena.addRectangle(felt);
         //Outlay
-        Rectangle powerBar = new Rectangle(500.00,850.00,500.00,50.00,"RED");
+        Rectangle powerBar = new Rectangle(650.00,850.00,500.00,50.00,"RED");
         arena.addRectangle(powerBar);
-        Line headSpot = new Line(500.00,251.99,500.00,748.99,5.00,"WHITE",3);
+        Line headSpot = new Line(650.00,251.99,500.00,748.99,5.00,"WHITE",3);
         arena.addLine(headSpot);
 
 
