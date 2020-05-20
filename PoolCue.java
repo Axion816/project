93edac4f524b3ad5955b02c1e411eaf8ecea69e0
Axion816 <60110445+Axion816@users.noreply.github.com>
2020-05-20 16:40:56 +0100

    Add files via upload

--- PoolCue.java
+++ PoolCue.java
@@ -3,24 +3,40 @@ public class PoolCue{
     private Rectangle powerDisplay;
     private double maxVelocity = 10.00;
     private boolean setup = true; 
+    private double width=0;
+    private double powerY=0;
     public void changePower(GameArena a,Ball cueBall){
         powerDisplay = new Rectangle(650.00,850.00,0.00,30.00,"RED",1);
+        a.addRectangle(powerDisplay);
         while(setup == true){
-            a.pause();
             if(a.rightPressed()==true){
                 if(power<1.00){
                     power = power + 0.10;
+                    width = width + 50;
+                    powerDisplay.setWidth(width);
                 }
             }
             if(a.leftPressed()==true){
                 if(power>0.00){
                     power = power + 0.10;
+                    width = width - 50;
+                    powerDisplay.setWidth(width);
                 }
             }
+            if(a.upPressed()==true){
+                powerY = powerY - 0.10;
+            }
+            if(a.downPressed()==true){
+                powerY = powerY + 0.10;
+            }
             double velocityX = power*10.00;
-            cueBall.setVelocity(velocityX,0.00);
+            double velocityY = powerY*10.00;
+            cueBall.setVelocity(velocityX,velocityY);
             if(a.spacePressed()==true){
                 setup = false;
+            }
+            for(int i=0;i<7;i++){
+                a.pause();
             }
         }
         return;
